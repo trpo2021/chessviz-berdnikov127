99ace4520ce6f39b963ddd51ac23b44da0aac208
Бердников Александр Павлович <ip011s06@bwp09.csc.local>
2021-04-24 11:10:45 +0700

    edit

--- src/chessviz/main.cpp
+++ src/chessviz/main.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 #include <libchessviz/locate.h>
 #include <libchessviz/output.h>
-=======
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <iostream>
->>>>>>> lab-3
 using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
     char chessboard[11][11]
             = {"1rnbqkbnr",
                "2pppppppp",
@@ -28,10 +25,9 @@ int main()
     return 0;
 }
 
-=======
 
     cout << "8 r n b q k b n r\n7 p p p p p p p p\n6\n5\n4\n3\n2 P P P P P P P P\n1 R N B Q K B N R\n  a b c d e f g h " << endl;
 
     return 0;
 }
->>>>>>> lab-3
+
