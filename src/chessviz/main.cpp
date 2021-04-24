c6d3d6a885db660faa70171ccf2a437fb35223a6
Бердников Александр Павлович <ip011s06@bwp09.csc.local>
2021-04-24 11:14:22 +0700

    edit

--- src/chessviz/main.cpp
+++ src/chessviz/main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <libchessviz/locate.h>
-#include <libchessviz/output.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <iostream>
+#include <libchessviz/vivod.h>
 using namespace std;
 
 int main()
@@ -18,16 +14,9 @@ int main()
                "6        ",
                "7PPPPPPPP",
                "8RNBQKBNR"};
-    output(chessboard);
+    vivod(chessboard);
     while (1) {
         locate(chessboard);
     }
     return 0;
 }
-
-
-    cout << "8 r n b q k b n r\n7 p p p p p p p p\n6\n5\n4\n3\n2 P P P P P P P P\n1 R N B Q K B N R\n  a b c d e f g h " << endl;
-
-    return 0;
-}
-
